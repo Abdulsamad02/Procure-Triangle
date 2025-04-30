bf99bff8c6dc0e6cfa20af3a390a0612ba9491a3
Farouk Adedamola <faroukadedamola1@gmail.com>
2025-04-30 11:55:10 +0100

    update fixes

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from '../ui/container';
 import { Mail, Phone, MapPin, Facebook, Twitter, Instagram, Linkedin } from 'lucide-react';
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Container } from '../ui/container';
 import { Button } from '../ui/button';
